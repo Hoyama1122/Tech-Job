b62f236b6d1184c4229fc07c4f9896f3c72b6bdb
Napat Phon <npat.pdn@gmail.com>
2025-11-23 12:21:17 +0700

    Update page.tsx

--- frontend/app/supervisor/work/page.tsx
+++ frontend/app/supervisor/work/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 import React, { useEffect, useState } from "react";
-import Allwork from "./AllWork";
+
 import { Users } from "@/lib/Mock/UserMock";
 
 const page = () => {
   
   return (
     <div>
-      <Allwork />
+  
     </div>
   );
 };
