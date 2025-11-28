908d4ddaab104251d79e54f3089f4990d7e4f596
Kkxnonn <suphitchaya.cc@gmail.com>
2025-11-28 10:29:37 +0700

    setting

--- frontend/app/technician/settings/page.tsx
+++ frontend/app/technician/settings/page.tsx
@@ -1,23 +1,58 @@
 "use client";
+import Link from "next/link";
 import React from "react";
 
 const Settings: React.FC = () => {
   return (
-    <div>
-      <h1>Settings Page for Technician</h1>
+    <div className="p-4 mx-auto max-w-md">
+      
+      <h1 className="text-2xl font-bold mb-6 text-gray-800 border-b pb-2">
+        การตั้งค่า (Settings)
+      </h1>
 
-      <div>
-        <button className="bg-gray-300 rounded-sm p-2 w-auto mt-4">
-          ศูนย์ช่วยเหลือ
-        </button>
-        <br />
-        <button className="bg-gray-300 rounded-sm p-2 w-auto mt-4">
-          นโยบายความเป็นส่วนตัว
-        </button>
-        <br />
-        <button className="bg-gray-300 rounded-sm p-2 w-auto mt-4">
-          ข้อกำหนดและเงื่อนไข
-        </button>
+      <div className="flex flex-col space-y-4">
+        
+        <div className="w-full">
+          <Link 
+            href={'/technician/settings/help'} 
+            className="flex justify-between items-center w-full bg-white text-gray-800 border border-gray-200 rounded-lg p-4 transition duration-200 ease-in-out shadow-sm hover:shadow-md hover:bg-gray-50"
+          >
+            <span className="font-medium text-lg">
+              ศูนย์ช่วยเหลือ
+            </span>
+            <span className="text-gray-400">
+              {/* สัญลักษณ์ลูกศรชี้ไปด้านขวา */}
+              &gt;
+            </span>
+          </Link>
+        </div>
+
+        <div className="w-full">
+          <button 
+            className="flex justify-between items-center w-full bg-white text-gray-800 border border-gray-200 rounded-lg p-4 transition duration-200 ease-in-out shadow-sm hover:shadow-md hover:bg-gray-50"
+          >
+            <Link href={'/technician/settings/privacy'} className="font-medium text-lg">
+              นโยบายความเป็นส่วนตัว
+            </Link>
+            <span className="text-gray-400">
+              &gt;
+            </span>
+          </button>
+        </div>
+        
+        <div className="w-full">
+          <button 
+            className="flex justify-between items-center w-full bg-white text-gray-800 border border-gray-200 rounded-lg p-4 transition duration-200 ease-in-out shadow-sm hover:shadow-md hover:bg-gray-50"
+          >
+            <Link href={'/technician/settings/terms'} className="font-medium text-lg">
+              ข้อกำหนดและเงื่อนไข
+            </Link>
+            <span className="text-gray-400">
+              &gt;
+            </span>
+          </button>
+        </div>
+
       </div>
     </div>
   );
