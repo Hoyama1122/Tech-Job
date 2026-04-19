739ae187b06efca5a9dd78cbaeef3fe9110c7635
Kkxnonn <suphitchaya.cc@gmail.com>
2026-04-20 02:14:45 +0700

    profile user
    แก้ไขโปรไฟล์ แต่ติดบัคยังเข้าไปเช็คไม่ได้ ที่แก้ไขหรือเพิ่มไปมีไฟล์ดังนี้
    - profile controller 2
    - user controller 2
    - middleware
    - profile routes
    - user routes
    - เพิ่มโฟลเดอร์ profile ใน app frontend และเพิ่ม page.tsx ใน technician

---
+++ frontend/app/(protected)/technician/page.tsx
@@ -0,0 +1,15 @@
+import Dashboard from "@/components/Technician/Dashboard/Dashboard";
+import React from "react";
+
+export const metadata = {
+  title: "Technician",
+  description: "A modern ecommerce platform",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
